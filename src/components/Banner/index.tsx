236eea21427f6bdfba3f6a8ecaf975f17de5c1bd
Leo Ribas <leoribasnasc@gmail.com>
2022-04-27 19:41:21 -0300

    Cria componente de Banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import * as S from './styles'
+import Button from '../Button'
+import Image from '../../assets/banner_image.png'
+
+const Banner = () => {
+  return (
+    <S.Container>
+      <S.BannerInfoWrapper>
+        <S.MainTitle>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam
+        </S.MainTitle>
+        <S.SubTitle>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet.
+        </S.SubTitle>
+        <Button text="Faça uma Simulação" />
+      </S.BannerInfoWrapper>
+      <S.BannerImage src={Image} />
+    </S.Container>
+  )
+}
+
+export default Banner
---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+`
+
+export const BannerInfoWrapper = styled.div``
+
+export const MainTitle = styled.h1``
+
+export const SubTitle = styled.h2``
+
+export const BannerImage = styled.img`
+  width: 330px;
+  height: 525.43px;
+`
