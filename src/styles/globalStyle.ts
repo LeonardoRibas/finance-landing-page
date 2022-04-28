a3d199c317ba8a0576919a6e33380c9e0968fcf7
Leo Ribas <leoribasnasc@gmail.com>
2022-04-28 12:21:02 -0300

    Adiciona estilos globais

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -2,7 +2,13 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    margin: 0
+    margin: 0;
+    font-family: 'Lexend', sans-serif;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
   }
 `
 export default GlobalStyle
