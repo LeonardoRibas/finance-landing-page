01b520c21ef9a30037690a89a6ac1b667393b1e7
Leo Ribas <leoribasnasc@gmail.com>
2022-04-27 21:26:04 -0300

    Adiciona componentes na landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import GlobalStyle from './globalStyle'
+import BannerSection from './components/BannerSection'
+import Header from './components/Header'
+import ServicesSection from './components/ServicesSection'
+import AboutUsSection from './components/AboutUsSection'
+import ContactSection from './components/ContactSection'
+import Footer from './components/Footer'
 
 function App() {
-  return <div>Olá mundo!</div>
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <BannerSection />
+      <ServicesSection />
+      <AboutUsSection />
+      <ContactSection />
+      <Footer />
+    </>
+  )
 }
 
 export default App
