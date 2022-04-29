f82069009f4c505ded732e3bf81ba336ae6aa527
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 10:14:49 -0300

    Estiliza secao de sobre nós

--- src/components/AboutUsSection/styles.ts
+++ src/components/AboutUsSection/styles.ts
@@ -2,25 +2,37 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  padding: 56px 0 56px 0;
+  margin-bottom: 56px;
 `
 
 export const Image = styled.img`
-  width: 537px;
-  height: 346px;
+  height: auto;
+  object-fit: contain;
+  min-width: 500px;
+  max-width: 700px;
+  margin-right: 140px;
 `
 
-export const TextInfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
+export const TextInfoWrapper = styled.div``
+
+export const Title = styled.h1`
+  font-size: 48px;
+  font-weight: 600;
+  color: ${(props) => props.theme.color.secondary};
+  margin-bottom: 24px;
 `
 
-export const Title = styled.h1``
+export const Text = styled.p`\
+  font-size: 20px;
+  font-weight: 400;
+  color: ${(props) => props.theme.color.text};
 
-export const Text = styled.p``
+`
 
 export const Divider = styled.div`
   width: 175px;
   height: 2px;
-  background-color: black;
+  background-color: ${(props) => props.theme.color.primary};
+  margin-bottom: 24px;
 `
