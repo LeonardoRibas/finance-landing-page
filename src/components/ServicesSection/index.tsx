04841722214c2a7759610638faff37cf257a364e
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 02:50:45 -0300

    Estiliza secao de servicos

--- src/components/ServicesSection/index.tsx
+++ src/components/ServicesSection/index.tsx
@@ -2,22 +2,25 @@ import React from 'react'
 import ServiceCard from '../ServiceCard'
 import * as S from './styles'
 import { MdSupervisorAccount, MdSecurity, MdLeaderboard } from 'react-icons/md'
+import theme from '../../styles/theme'
 
 const ServicesSection = () => {
   return (
     <S.Container>
       <ServiceCard
-        icon={() => <MdSupervisorAccount size={56} />}
+        icon={() => (
+          <MdSupervisorAccount size={56} color={theme.color.secondary} />
+        )}
         title="Suporte"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor"
       />
       <ServiceCard
-        icon={() => <MdSecurity size={56} />}
+        icon={() => <MdSecurity size={56} color={theme.color.secondary} />}
         title="Segurança"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor"
       />
       <ServiceCard
-        icon={() => <MdLeaderboard size={56} />}
+        icon={() => <MdLeaderboard size={56} color={theme.color.secondary} />}
         title="Análise"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor"
       />
--- src/components/ServicesSection/styles.ts
+++ src/components/ServicesSection/styles.ts
@@ -2,4 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  flex: 1;
+  justify-content: space-between;
+  padding: 56px 0 56px 0;
+  margin-bottom: 56px;
 `
