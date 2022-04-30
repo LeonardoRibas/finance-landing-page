90662f2250325981ee347473077a69988236e638
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 22:40:02 -0300

    Estiliza secao de contato

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 import ContactInfo from '../ContactInfo'
 import Form from '../Form'
 import * as S from './styles'
-import { FiPhone, FiMail, FiMapPin } from 'react-icons/fi'
+import {
+  FiPhone,
+  FiMail,
+  FiMapPin,
+  FiInstagram,
+  FiFacebook,
+  FiLinkedin,
+} from 'react-icons/fi'
+import theme from '../../styles/theme'
 
 const ContactSection = () => {
   return (
@@ -13,17 +21,22 @@ const ContactSection = () => {
           Preencha o formulário e entraremos em contato com você
         </S.SubTitle>
         <ContactInfo
-          icon={() => <FiPhone />}
+          icon={() => <FiPhone size={24} color={theme.color.text} />}
           text="+55 (61) 3081-5555 ou +55 (61) 99249-0548"
         />
         <ContactInfo
-          icon={() => <FiMail />}
+          icon={() => <FiMail size={24} color={theme.color.text} />}
           text="guerra@guerraconsorcios.com.br"
         />
         <ContactInfo
-          icon={() => <FiMapPin />}
+          icon={() => <FiMapPin size={24} color={theme.color.text} />}
           text="The Union Office (Lt 3, Torre E, sala 06) EPIA Trecho 3 - Asa Sul, Brasília - DF "
         />
+        <S.SocialMediaWrapper>
+          <FiInstagram size={24} color={theme.color.text} />
+          <FiFacebook size={24} color={theme.color.text} />
+          <FiLinkedin size={24} color={theme.color.text} />
+        </S.SocialMediaWrapper>
       </S.TextInfoWrapper>
       <Form />
     </S.Container>
