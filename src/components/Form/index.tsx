b85949dd739b3200f25ebbdf30ab4ba8917b2885
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 22:39:51 -0300

    Estiliza o Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import * as S from './styles'
 import Button from '../Button'
-import TextField from '@mui/material/TextField'
 
 const Form = () => {
   return (
     <S.Container>
-      <TextField id="standard-basic" label="Nome Completo" variant="standard" />
-      <TextField id="standard-basic" label="Email" variant="standard" />
-      <TextField id="standard-basic" label="Telefone" variant="standard" />
-      <TextField
+      <S.CustomTextField
+        id="standard-basic"
+        label="Nome Completo"
+        variant="standard"
+      />
+      <S.CustomTextField id="standard-basic" label="Email" variant="standard" />
+      <S.CustomTextField
+        id="standard-basic"
+        label="Telefone"
+        variant="standard"
+        placeholder="11912341234"
+      />
+      <S.CustomTextField
         id="standard-multiline-static"
         label="Mensagem"
         multiline
         rows={5}
         variant="standard"
       />
-      <Button text="Enviar Mensagem" />
+      <S.ButtonDiv>
+        <Button
+          text="Enviar Mensagem"
+          variant="primary"
+          onClick={(e) => {
+            e.preventDefault()
+          }}
+        />
+      </S.ButtonDiv>
     </S.Container>
   )
 }
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,6 +1,49 @@
 import styled from 'styled-components'
+import TextField from '@mui/material/TextField'
+import theme from '../../styles/theme'
 
 export const Container = styled.form`
   display: flex;
+  flex: 4;
   flex-direction: column;
+  justify-content: space-between;
 `
+
+export const ButtonDiv = styled.div`
+  width: 50%;
+`
+
+export const CustomTextField = styled(TextField)({
+  '& .MuiInputBase-input': {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+
+  '& label.Mui-focused': {
+    color: theme.color.secondary,
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: theme.color.primary,
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'red',
+    },
+    '&:hover fieldset': {
+      borderColor: 'yellow',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+  },
+})
