46c2dd6d1472cce2217438f288c30bd31fb39f15
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 22:40:02 -0300

    Estiliza secao de contato

--- src/components/ContactSection/styles.ts
+++ src/components/ContactSection/styles.ts
@@ -4,10 +4,31 @@ export const Container = styled.div`
   display: flex;
 `
 
-export const TextInfoWrapper = styled.div``
+export const TextInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 3;
+  margin-right: 140px;
+`
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+  font-size: 32px;
+  font-weight: 700;
+  color: ${(props) => props.theme.color.secondary};
+  margin-bottom: 32px;
+`
 
-export const SubTitle = styled.p``
+export const SubTitle = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${(props) => props.theme.color.text};
+  margin-bottom: 48px;
+`
 
-export const SocialMediaWrapper = styled.div
+export const SocialMediaWrapper = styled.div`
+  display: flex;
+  width: 50%;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 40px;
+`
