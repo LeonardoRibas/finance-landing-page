837f875c0cd093e068da890eb9e5148f649f222b
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 22:39:42 -0300

    Estiliza ContactInfo

--- src/components/ContactInfo/styles.ts
+++ src/components/ContactInfo/styles.ts
@@ -2,6 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  align-items: center;
+  margin-bottom: 24px;
 `
 
-export const Text = styled.p``
+export const Text = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${(props) => props.theme.color.text};
+  margin-left: 16px;
+`
