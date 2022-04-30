d18d7f015413af8b63b07c2b33d13697492f0cef
Leo Ribas <leoribasnasc@gmail.com>
2022-04-30 15:16:25 -0300

    Estiliza Footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,11 +2,22 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.theme.color.secondary};
+  margin-top: 56px;
+  padding-top: 16px;
+  padding-bottom: 16px;
 `
 
 export const Logo = styled.img`
   width: 100px;
   height: 30.84px;
+  margin-right: 56px;
 `
 
-export const Text = styled.p``
+export const Text = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${(props) => props.theme.color.white};
+`
