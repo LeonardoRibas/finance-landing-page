3fa49f36bf749932b0a88915f87a00bf3316d5b2
Leo Ribas <leoribasnasc@gmail.com>
2022-04-29 22:39:10 -0300

    Adiciona onClick em Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,11 +5,17 @@ type ButtonProps = {
   variant?: 'primary' | 'secondary'
   size?: 'large' | 'small'
   text: string
+  onClick?: (e: any) => void
 }
 
-const Button = ({ variant = 'primary', size = 'small', text }: ButtonProps) => {
+const Button = ({
+  variant = 'primary',
+  size = 'small',
+  text,
+  onClick,
+}: ButtonProps) => {
   return (
-    <S.ButtonWrapper variant={variant} size={size}>
+    <S.ButtonWrapper variant={variant} size={size} onClick={onClick}>
       <S.Text>{text}</S.Text>
     </S.ButtonWrapper>
   )
